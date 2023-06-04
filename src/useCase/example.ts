e9441103515ff077e4046ffcf2802014eac87e6b
Vincent Vella <vincevella0@gmail.com>
2023-06-03 20:22:01 -0400

    feat: update to preferred local setup

--- src/useCase/example.ts
+++ src/useCase/example.ts
@@ -1,19 +1,18 @@
-
 export class Guesser {
-  private word: string = '';
-  private correctGuesses = 0;
-  
-  public setWord (word: string) {
-    this.word = word;
+  private word = ''
+  private correctGuesses = 0
+
+  public setWord(word: string) {
+    this.word = word
   }
 
-  public guessWord (word: string): boolean {
-    let correct = word.toLowerCase() === this.word.toLowerCase();
-    if (correct) this.correctGuesses++;
-    return correct;
+  public guessWord(word: string): boolean {
+    const correct = word.toLowerCase() === this.word.toLowerCase()
+    if (correct) this.correctGuesses++
+    return correct
   }
 
-  public getNumberCorrectGuesses (): number {
-    return this.correctGuesses;
+  public getNumberCorrectGuesses(): number {
+    return this.correctGuesses
   }
 }
